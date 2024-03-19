c03276b795eb6ed00011e70929f93b7851d820d7
Oliver Jenkins <cyberforge1@gmail.com>
2024-03-19 15:47:54 +1100

    addition of dynamic code printing

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 let typed = new Typed(".auto-type", {
-    strings: ["Travelling", "Making movies", "Playing Sports", "Reading", "Building projects", "Creative design"],
+    strings: ["Travelling", "Making movies", "Playing sports", "Reading", "Building projects", "Creative design"],
     typeSpeed: 150, 
     backSpeed: 150,
     loop: true
